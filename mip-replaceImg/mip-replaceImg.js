f5d90795f8ea25ae211d405767d3ef37b160f515
axl <364857081@qq.com>
2016-11-09 10:53:42 +0800

    修改页面中的img标签为mip-img标签

---
+++ mip-replaceImg/mip-replaceImg.js
@@ -0,0 +1,26 @@
+/**
+ * @file mip-demo 组件
+ * @author
+ */
+
+define(function (require) {
+
+    var customElement = require('customElement').create();
+
+    /**
+     * 构造元素，只会运行一次
+     */
+    customElement.prototype.build = function () {
+        window.onload=function(){
+			var body_dom = document.getElementsByTagName('body')[0];
+			var img_mip=img_replace(body_dom.innerHTML.toString());
+			body_dom.innerHTML = img_mip;
+			console.log(img_mip)
+		}
+		function img_replace(str){
+			return str.replace(/<img/g,'<mip-img');
+		}
+    };
+
+    return customElement;
+});
